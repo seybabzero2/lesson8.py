23d1ae045d1205e8bed404123f91ed1c867c5d4f
seybabzero2 <95244442+seybabzero2@users.noreply.github.com>
2022-11-09 18:08:38 +0200

    Add files via upload

---
+++ urok8.py
@@ -0,0 +1,34 @@
+# import logging
+#
+# logging.basicConfig(level = logging.DEBUG,
+#                     filename = "logs.log",
+#                     filemode = "a",
+#                     format = "We have next logging message:%(asctime)s:%(levelname)s - %(message)s")
+# logging.debug("debug")
+# logging.info("info")
+# logging.warning("warning")
+# logging.error("error")
+# logging.critical("critical")
+#
+# try:
+#     print(10/0)
+# except Exception:
+#     logging.exception("Exception")
+
+# assert 2+2==5, "ne sobralos"
+
+# """
+# >>> 2+2
+# 5
+# """
+# if __name__ == "__main__":
+#     import doctest
+#     doctest.testmod()
+
+def adder(*args, **kwargs):
+    result = 0
+    for _ in args:
+        result += _
+    for _ in kwargs.values():
+        result += _
+    return result
